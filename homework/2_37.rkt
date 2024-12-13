07ce34db0c6ad3b9852d967b207e15b995ddc8a7
Dimitri Krassovski <dimitri@wix.com>
2024-12-13 11:11:49 +0000

    queens and matrices

--- homework/2_37.rkt
+++ homework/2_37.rkt
@@ -8,13 +8,39 @@
           (accumulate op initial (cdr sequence)))))
 
 
-(define (append seq1 seq2) (accumulate cons seq2 seq1))
+(define (accumulate-n op initial seqs) 
+    (if (null? (car seqs))
+        nil
+        (cons (accumulate op initial (map car seqs)) 
+              (accumulate-n op initial (map cdr seqs)))))
 
-(append (list 2 4) (list 4 6))
+(define (dot-product v w)
+    (accumulate + 0 (map * v w)))
 
-(define (length sequence) (accumulate (lambda (_ y) (+ 1 y) ) 0 sequence))
-(length (list 1 3 7 66 6))
+(define (matrix-*-vector m v)
+   (map (lambda (x) (dot-product v x)) m))
 
-(define (map p sequence)
-    (accumulate (lambda (x y)  (cons (p x) y)) nil sequence))
-(map double (list 1 3 7))
+
+(define (transpose mat) 
+    (accumulate-n cons nil mat))
+
+
+(define (matrix-*-matrix m n) 
+    (let ((cols (transpose n)))
+          (map (lambda (row) (map (lambda (col) (dot-product row col)) cols)) m)))
+
+(define (matrix-*-matrix2 m n) 
+    (let ((cols (transpose n)))
+          (map (lambda (row) (matrix-*-vector cols row)) m)))
+
+
+(define mat '((1 2 3 4) (4 5 6 6) (6 7 8 9)))
+(define mat2 '((0 1) (2 3) (3 5) (0 0)))
+mat
+(transpose mat2)
+(transpose mat)
+;mat
+(define vec '(0 1 0 4))
+(matrix-*-vector mat vec)
+(matrix-*-matrix mat mat2)
+(matrix-*-matrix2 mat mat2)
---
+++ homework/2_42.rkt
@@ -0,0 +1,97 @@
+#lang sicp
+
+(define (double x) (* 2 x))
+
+(define (fold-right op initial sequence) (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+
+(define (fold-left op initial sequence) (define (iter result rest)
+(if (null? rest) result
+        (iter (op result (car rest))
+              (cdr rest))))
+(iter initial sequence))
+
+
+
+(define (reverse sequence)
+(fold-right (lambda (x y) (append y (list x))) nil sequence))
+(define (reversel sequence)
+(fold-left (lambda (x y) (cons y x)) nil sequence))
+
+(reverse '(1 3 5))
+(reversel '(1 6 9))
+
+(define (accumulate op initial sequence) (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (list-sum l) (accumulate + 0 l))
+(define (filter predicate sequence) (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+(filter predicate (cdr sequence)))) (else (filter predicate (cdr sequence)))))
+
+(define (remove item sequence)
+(filter (lambda (x) (not (= x item)))
+sequence))
+
+
+
+(define (flatmap proc seq)
+      (accumulate append nil (map proc seq)))
+
+
+(define (permutations s)
+      (if (null? s) ;emptyset?
+          (list nil) ;sequencecontainingemptyset 
+          (flatmap (lambda (x)
+                    (map (lambda (p) (cons x p)) (permutations (remove x s)))) s )))
+
+
+(permutations (list 2 4 7))
+
+(define (square n) (* n n))
+
+(define (enumerate-interval low high) (if (> low high)
+nil
+      (cons low (enumerate-interval (+ low 1) high))))
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1))))) (define (divides? a b) (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(and )
+
+(define (queens board-size) 
+  (define (empty-board) nil)
+  (define (safe? k p) 
+    (let ((ups (enumerate-interval 1 (- k 1))))
+      (and 
+        (accumulate (lambda (a b) (and a b)) #t (map (lambda (pos)  (not (= (car p) pos))) (cdr p)))
+        (accumulate (lambda (a b) (and a b)) #t (map (lambda (pos offset)  (not (= (car p) (+ offset pos)))) (cdr p) ups))
+        (accumulate (lambda (a b) (and a b)) #t (map (lambda (pos offset)  (not (= (car p) (- pos offset)))) (cdr p) ups)))
+      ))
+  (define (adjoin-position new-row k rest-of-queens) (cons new-row rest-of-queens))
+  (define (queen-cols k)
+    (if 
+      (= k 0)  
+      (list nil)
+      (filter (lambda (positions) (safe? k positions)) 
+        (flatmap
+          (lambda (rest-of-queens) 
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)) 
+                 )
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+(queens 4)
+(length (queens 8))
